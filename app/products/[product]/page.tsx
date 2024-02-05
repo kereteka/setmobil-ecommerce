de5223e758e3c5e101c7b8b779b2fef596f8cee6
kereteka <mehmet.sengun1@gmail.com>
2024-02-05 21:35:54 +0300

    ProductPage was made

--- app/products/[product]/page.tsx
+++ app/products/[product]/page.tsx
@@ -1,42 +1,14 @@
-// // pages/products/[id].tsx
+import ProductCard from '@/app/components/product-card';
 
-// import type { InferGetStaticPropsType, GetStaticProps, GetStaticPaths } from 'next';
+const fetchProduct = async (id: string | undefined) =>
+  fetch(`https://api.escuelajs.co/api/v1/products/${id}`, {
+    cache: 'no-store',
+  }).then((res) => res.json());
 
-// type Product = {
-//   id: number;
-//   title: string;
-//   price: number;
-//   description: string;
-//   category: {
-//     id: number;
-//     name: string;
-//     image: string;
-//   };
-//   images: string[];
-// };
+const Product = async ({ params }: { params?: { product: string } }) => {
+  const id = params?.product;
+  const product = (await fetchProduct(id)) || [];
+  return <ProductCard product={product} />;
+};
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const res = await fetch('https://api.escuelajs.co/api/v1/products');
-//   const products = await res.json();
-//   const paths = products.map((product: Product) => ({
-//     params: { id: product.id.toString() },
-//   }));
-
-//   return { paths, fallback: 'blocking' };
-// };
-
-// export const getStaticProps: GetStaticProps<{ product: Product }> = async (context) => {
-//   const { id } = context.params!;
-//   const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`);
-//   const product: Product = await res.json();
-//   return { props: { product } };
-// };
-
-// const ProductPage: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ product }) => (
-//   <div>
-//     <h1>{product.title}</h1>
-//     {/* Diğer ürün detayları */}
-//   </div>
-// );
-
-// export default ProductPage;
+export default Product;
