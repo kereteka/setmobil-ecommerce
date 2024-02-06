efcb487361c82d54abd04b9dc7b2a033b3264c71
kereteka <mehmet.sengun1@gmail.com>
2024-02-06 03:15:28 +0300

    added router in product-card and yarn install to solve vercel issue

--- app/components/product-card/index.tsx
+++ app/components/product-card/index.tsx
@@ -1,13 +1,18 @@
 'use client';
 
 import { Card, Text, Badge, Image, Group } from '@mantine/core';
+import { useRouter } from 'next/navigation';
 import { Product } from '@/types';
 import AddToCartButton from '../add-to-cart-button';
 
 export default function ProductCard({ product }: { product: Product }) {
   const { title, price, images } = product;
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/products/${product.id}`);
+  };
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
+    <Card shadow="sm" padding="lg" radius="md" withBorder onClick={handleClick}>
       <Card.Section>
         <Image src={images[0]} height={160} alt="Norway" />
       </Card.Section>
